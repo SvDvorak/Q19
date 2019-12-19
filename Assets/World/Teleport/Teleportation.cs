a93c31146202917a405008ebfc5e1079d14a5aef
Andreas Wilcox <andreas.wilcox@m-sys.se>
2019-12-19 11:13:12 +0100

    Made a bunch of prefabs and added teleport

---
+++ Assets/World/Teleport/Teleportation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleportation : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+            SceneManager.LoadScene("Level_1");
+    }
+}
