07ad42145ae3006e0efea812262d88b564c8a971
Andreas Wilcox <andreas.wilcox@m-sys.se>
2020-02-25 13:18:52 +0100

    Added enemy model (ugly and not done), a phat laser and made enemy killable

---
+++ Assets/Enemy/EnemyDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public Transform EnemyModel;
+    public ParticleSystem Gibs;
+
+    public void Kill()
+    {
+        EnemyModel.DOScale(0, 0.2f);
+        EnemyModel.DOLocalMoveY(1, 0.2f);
+        transform.tag = "Untagged";
+        Gibs.Play();
+    }
+}
---
+++ Assets/Player/Shooting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform View;
+
+    void FixedUpdate()
+    {
+        var ray = new Ray(View.position, View.forward*1000);
+
+        if (Input.GetMouseButton(0) && Physics.Raycast(ray, out var hit, float.MaxValue) && hit.transform.tag == "Hittable")
+        {
+            hit.transform.GetComponent<EnemyDeath>().Kill();
+        }
+    }
+}
