a34b093fb94f18c0adaed68e241a2ce47e8a7175
Andreas Wilcox <andreas.wilcox@m-sys.se>
2020-02-28 16:14:10 +0100

    Fixed an issue in SineVFX where laser is shown on first frame in game

---
+++ Assets/Libraries/SineVFX/Volumetric3DLasers/Scripts/MouseTargetV3D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTargetV3D : MonoBehaviour {
+
+    public Transform targetCursor;
+    public float speed = 1f;
+
+    private Vector3 mouseWorldPosition;
+
+    private void FixedUpdate ()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            mouseWorldPosition = hit.point;
+        }
+
+        Quaternion toRotation = Quaternion.LookRotation(mouseWorldPosition - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
+        targetCursor.position = mouseWorldPosition;
+
+    }
+}
